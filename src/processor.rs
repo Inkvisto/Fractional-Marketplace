02352b7f777fb96e5886170c9b48b2b38059d189
MihuNt3r <borovykmis@gmail.com>
2025-08-15 14:30:29 +0300

    Temporary fix for error with deserialization

--- src/processor.rs
+++ src/processor.rs
@@ -1,3 +1,4 @@
+use solana_program::msg;
 use {
     borsh::{BorshDeserialize, BorshSerialize},
     solana_program::{account_info::AccountInfo, entrypoint::ProgramResult, pubkey::Pubkey},
@@ -19,14 +20,15 @@ pub fn process_instruction(
     accounts: &[AccountInfo],
     instruction_data: &[u8],
 ) -> ProgramResult {
-    // let instruction = FractionalMarketplaceInstruction::try_from_slice(instruction_data)?;
+    let instruction = FractionalMarketplaceInstruction::try_from_slice(instruction_data);
 
-    let args = LockNFTArgs {};
-    
-    lock_nft(program_id, accounts, args)
+    let instruction = instruction.unwrap_or_else(|err| {
+        msg!("Error in deserialization: {}", err);
+        FractionalMarketplaceInstruction::Lock(LockNFTArgs {})
+    });
 
-    // match instruction {
-    //     FractionalMarketplaceInstruction::Fractionalize(args) => fractionalize_nft(accounts, args),
-    //     FractionalMarketplaceInstruction::Lock(args) => lock_nft(program_id, accounts, args),
-    // }
+    match instruction {
+        FractionalMarketplaceInstruction::Fractionalize(args) => fractionalize_nft(accounts, args),
+        FractionalMarketplaceInstruction::Lock(args) => lock_nft(program_id, accounts, args),
+    }
 }
