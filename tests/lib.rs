e53ee06f5c8e67e2a652db98dbfee37c07467249
MihuNt3r <borovykmis@gmail.com>
2025-08-18 20:28:49 +0300

    feat: add test to invoke deployed smart contract

--- tests/lib.rs
+++ tests/lib.rs
@@ -1,8 +1,12 @@
+use std::str::FromStr;
+use spl_associated_token_account::processor;
 use solana_program_test::*;
 use solana_sdk::{instruction::{AccountMeta, Instruction}, pubkey::Pubkey, signature::Signer, system_instruction, system_program, transaction::Transaction};
 use solana_sdk::program_pack::Pack;
-use solana_sdk::signature::Keypair;
+use solana_sdk::signature::{read_keypair_file, Keypair};
+use Fractional_Marketplace::instructions::FractionalizeNFTArgs;
 use Fractional_Marketplace::processor::FractionalMarketplaceInstruction;
+use solana_client::rpc_client::RpcClient;
 
 #[tokio::test]
 async fn test_lock_nft() {
@@ -173,4 +177,40 @@ async fn test_lock_nft() {
     assert_eq!(token_account.amount, 1);
     // Checking that NFT was transferred from user NFT token account
     assert_eq!(user_nft_token_account.amount, 0);
+}
+
+#[tokio::test(flavor = "multi_thread", worker_threads = 4)]
+async fn test_deployed_program() {
+    let client = RpcClient::new("http://127.0.0.1:8899".to_string());
+
+    // Load payer
+    let payer = read_keypair_file("/home/misha/.config/solana/id.json").unwrap();
+
+    // Program ID from `solana program deploy`
+    let program_id = Pubkey::from_str("HUmJGJvqTVAJ9MeJRCJhL93FmaTpBUz1JzMTMdEwPVAM").unwrap();
+
+    let args = FractionalMarketplaceInstruction::Fractionalize(FractionalizeNFTArgs {
+        total_shares: 7,
+        nft_mint: Pubkey::new_unique(),
+    });
+    let data = borsh::to_vec(&args).unwrap();
+
+    // For now, no extra accounts are needed (just payer)
+    let instruction = Instruction::new_with_bytes(
+        program_id,
+        &data,
+        vec![AccountMeta::new(payer.pubkey(), true)],
+    );
+
+    // Send transaction
+    let recent_blockhash = client.get_latest_blockhash().unwrap();
+
+    let tx = Transaction::new_signed_with_payer(
+        &[instruction],
+        Some(&payer.pubkey()),
+        &[&payer],
+        recent_blockhash,
+    );
+
+    client.send_and_confirm_transaction(&tx).unwrap();
 }
